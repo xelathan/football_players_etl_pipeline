d46ee4f6ef493102ed70b356038ede8648e7db19
xelathan <alex.t.tran@gmail.com>
2025-01-11 23:11:46 -0800

    Added updating

--- airflow/dags/football_players_dag.py
+++ airflow/dags/football_players_dag.py
@@ -55,7 +55,7 @@ def extract_from_football_api(**kwargs):
                 
                 # handle error
                 if 'errors' in data and len(data['errors']) > 0:
-                    raise Exception(data['errors'])
+                    break
                 
                 # handle pagination
                 if 'paging' in data and 'current' in data['paging']:
@@ -157,11 +157,13 @@ def parquet_parser(filepath):
     Args:
         filepath: Path to the parquet file in S3 (downloaded to local temp storage by operator)
     Returns:
-        Iterator of rows from the parquet file
+        Iterator of rows from the parquet file, where each row corresponds to a single database record.
     """
+    # Load the parquet file into a DataFrame
     df = pd.read_parquet(filepath)
-        
-    for row in df.values:
+    
+    # Convert each row of the DataFrame into a list and yield
+    for _, row in df.iterrows():
         yield row.tolist()
 
 def get_root_failed_task(context):
@@ -466,7 +468,7 @@ with DAG(
         task_id='create_table_if_needed',
         sql="""
             CREATE TABLE IF NOT EXISTS {{ params.table_name }} (
-                player_id SERIAL PRIMARY KEY,
+                player_id INT,
                 player_name VARCHAR(255),
                 player_firstname VARCHAR(255),
                 player_lastname VARCHAR(255),
@@ -533,18 +535,116 @@ with DAG(
           show_return_value_in_logs=True,
     )
 
-    load_processed_records_to_postgres = S3ToSqlOperator(
+    create_staging_table = SQLExecuteQueryOperator(
+        task_id='create_staging_table',
+        sql="""
+        CREATE TABLE staging_table (
+            player_id INT,
+            player_name VARCHAR(255),
+            player_firstname VARCHAR(255),
+            player_lastname VARCHAR(255),
+            player_age INT,
+            player_birth_date VARCHAR(255),
+            player_birth_place VARCHAR(255),
+            player_birth_country VARCHAR(255),
+            player_nationality VARCHAR(255),
+            player_height VARCHAR(255),
+            player_weight VARCHAR(255),
+            player_injured BOOLEAN,
+            player_photo VARCHAR(255),
+            cards_red INT,
+            cards_yellow INT,
+            cards_yellowred INT,
+            dribbles_attempts FLOAT,
+            dribbles_past INT,
+            dribbles_success FLOAT,
+            duels_total FLOAT,
+            duels_won FLOAT,
+            fouls_committed FLOAT,
+            fouls_drawn FLOAT,
+            games_appearences INT,
+            games_captain BOOLEAN,
+            games_lineups INT,
+            games_minutes INT,
+            games_number FLOAT,
+            games_position VARCHAR(255),
+            games_rating VARCHAR(255),
+            goals_assists FLOAT,
+            goals_conceded INT,
+            goals_saves INT,
+            goals_total INT,
+            league_country VARCHAR(255),
+            league_flag VARCHAR(255),
+            league_id INT,
+            league_logo VARCHAR(255),
+            league_name VARCHAR(255),
+            league_season INT,
+            passes_accuracy FLOAT,
+            passes_key FLOAT,
+            passes_total FLOAT,
+            penalty_commited FLOAT,
+            penalty_missed FLOAT,
+            penalty_saved FLOAT,
+            penalty_scored FLOAT,
+            penalty_won FLOAT,
+            shots_on FLOAT,
+            shots_total FLOAT,
+            substitutes_bench INT,
+            substitutes_in INT,
+            substitutes_out INT,
+            tackles_blocks FLOAT,
+            tackles_interceptions FLOAT,
+            tackles_total FLOAT,
+            team_id INT,
+            team_logo VARCHAR(255),
+            team_name VARCHAR(255)
+        );
+        """,
+        conn_id="aws_rds_postgres_conn",
+        show_return_value_in_logs=True,
+    )
+
+    load_processed_records_to_staging_table = S3ToSqlOperator(
         task_id="load_to_rds",
         s3_bucket=config["FOOTBALL_DATA_BUCKET"],
         s3_key="{{ ti.xcom_pull(task_ids='get_processed_file_path_name') }}",
         schema='public',
         aws_conn_id="aws_default",
-        table=f"football_players_data_{config['SEASON']}",
+        table="staging_table",
         sql_conn_id="aws_rds_postgres_conn",
         column_list=[],
         parser=parquet_parser,
     )
+
+    # merge tables
+    merge_staging_with_production_table = SQLExecuteQueryOperator(
+        task_id="merge_staging_with_production_table",
+        sql="""
+        INSERT INTO {{ params.production_table }}
+        SELECT *
+        FROM staging_table
+        ON CONFLICT (player_id, team_id, league_id, league_season)
+        DO UPDATE SET
+            player_id = EXCLUDED.player_id,
+            team_id = EXCLUDED.team_id,
+            league_id = EXCLUDED.league_id,
+            league_season = EXCLUDED.league_season;
+        """,
+        conn_id="aws_rds_postgres_conn",
+        params={"production_table": f"football_players_data_{config["SEASON"]}"},
+        show_return_value_in_logs=True,
+    )
+
+
+    delete_staging_table = SQLExecuteQueryOperator(
+        task_id='delete_staging_table',
+        sql="""DROP TABLE IF EXISTS staging_table;""",
+        conn_id="aws_rds_postgres_conn",
+        show_return_value_in_logs=True,
+    )
     
     extract_football_api >> store_unprocessed_records_s3 >> create_emr_serverless_app \
     >> start_emr_job >> delete_emr_serverless_app >> get_processed_file_path_name \
-    >> create_table_if_needed >> load_processed_records_to_postgres
+    >> create_table_if_needed >> create_staging_table >> load_processed_records_to_staging_table \
+    >> merge_staging_with_production_table >> delete_staging_table
+    
